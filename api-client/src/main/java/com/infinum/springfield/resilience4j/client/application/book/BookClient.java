30385eace7bff414e9cf8057d340c3fa9e8d4539
Dominik Hodja <uc0133@scorvelogica.com>
2025-07-10 10:04:08 +0200

    Add async fetching to API client

--- api-client/src/main/java/com/infinum/springfield/resilience4j/client/application/book/BookClient.java
+++ api-client/src/main/java/com/infinum/springfield/resilience4j/client/application/book/BookClient.java
@@ -1,8 +1,11 @@
 package com.infinum.springfield.resilience4j.client.application.book;
 
 import com.infinum.springfield.resilience4j.client.application.shared.PagedResponse;
+import java.util.concurrent.CompletableFuture;
 
 public interface BookClient {
 
     PagedResponse<BookDto> getBooks(int page, int size);
+
+    CompletableFuture<PagedResponse<BookDto>> getBooksAsync(int page, int size);
 }
--- api-client/src/main/java/com/infinum/springfield/resilience4j/client/infrastructure/http/BookRestClient.java
+++ api-client/src/main/java/com/infinum/springfield/resilience4j/client/infrastructure/http/BookRestClient.java
@@ -5,13 +5,17 @@ import com.infinum.springfield.resilience4j.client.application.book.BookDto;
 import com.infinum.springfield.resilience4j.client.application.shared.PagedResponse;
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 @Component
 class BookRestClient implements BookClient {
@@ -29,8 +33,9 @@ class BookRestClient implements BookClient {
         this.restClient = restClient;
     }
 
-    @Retry(name = API_SERVER, fallbackMethod = "fallbackGetBooks")
+    @Retry(name = API_SERVER, fallbackMethod = "fallbackGetBooksWithFullSignature")
     @CircuitBreaker(name = API_SERVER)
+    @RateLimiter(name = API_SERVER)
     @Bulkhead(name = API_SERVER)
     @Override
     public PagedResponse<BookDto> getBooks(int page, int size) {
@@ -40,9 +45,37 @@ class BookRestClient implements BookClient {
                 .body(BOOKS_LIST_TYPE);
     }
 
-    private PagedResponse<BookDto> fallbackGetBooks(int page, int size, Exception ex) {
-        log.warn("Error executing HTTP request, recovering by returning null, error: {}", ex.getMessage());
+    @Override
+    @CircuitBreaker(name = API_SERVER, fallbackMethod = "fallbackGetBooksAsync")
+    @TimeLimiter(name = API_SERVER, fallbackMethod = "fallbackGetBooksAsync")
+    public CompletableFuture<PagedResponse<BookDto>> getBooksAsync(int page, int size) {
+        return new CompletableFuture<PagedResponse<BookDto>>()
+                .completeAsync(
+                        () -> getBooks(page, size),
+                        CompletableFuture.delayedExecutor(0, TimeUnit.SECONDS)
+                ).thenApply(books -> {
+                    log.info("Received {} books", books.content().size());
+                    return books;
+                });
+    }
+
+    private PagedResponse<BookDto> invalidFallbackGetBooks(int size, Exception ex) {
+        log.warn("Error executing HTTP request, recovering by returning empty page, error: {}", ex.getMessage());
         return new PagedResponse<>(List.of(), PagedResponse.PageMetadata.empty());
     }
 
+    private PagedResponse<BookDto> fallbackGetBooksWithFullSignature(int page, int size, Exception ex) {
+        log.warn("Error executing HTTP request [full signature], recovering by returning empty page, error: {}", ex.getMessage());
+        return new PagedResponse<>(List.of(), PagedResponse.PageMetadata.empty());
+    }
+
+    private PagedResponse<BookDto> fallbackGetBooksWithExceptionOnly(Exception ex) {
+        log.warn("Error executing HTTP request [exception only signature], recovering by returning empty page, error: {}", ex.getMessage());
+        return new PagedResponse<>(List.of(), PagedResponse.PageMetadata.empty());
+    }
+
+    private CompletableFuture<PagedResponse<BookDto>> fallbackGetBooksAsync(int page, int size, Exception ex) {
+        log.warn("Error executing HTTP request asynchronously, recovering by returning empty page, error: {}", ex.getMessage());
+        return CompletableFuture.completedFuture(new PagedResponse<>(List.of(), PagedResponse.PageMetadata.empty()));
+    }
 }
