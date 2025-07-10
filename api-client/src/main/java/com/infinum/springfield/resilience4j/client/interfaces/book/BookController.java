9bd4621bbbc5c4400ee5fb783c6b3fe146d3355b
Dominik Hodja <uc0133@scorvelogica.com>
2025-07-10 10:04:23 +0200

    Add async page endpoint to client

--- api-client/src/main/java/com/infinum/springfield/resilience4j/client/interfaces/book/BookController.java
+++ api-client/src/main/java/com/infinum/springfield/resilience4j/client/interfaces/book/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.util.concurrent.CompletableFuture;
 
 @Controller
 class BookController {
@@ -27,4 +28,18 @@ class BookController {
         model.addAttribute("pageMetadata", bookResponse.page());
         return "books";
     }
+
+    @GetMapping("/books-async")
+    CompletableFuture<String> getBooksAsync(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size,
+            Model model) {
+        return this.bookClient.getBooksAsync(page, size)
+                .thenAccept(response -> {
+                    model.addAttribute("books", response.content());
+                    model.addAttribute("pageMetadata", response.page());
+                })
+                .thenApply(v -> "books-async");
+    }
+
 }
